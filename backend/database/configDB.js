d520055a728323dde3ad825e918de83c7835a3f9
Pierre-Alexandre <161696548+AydenGrace@users.noreply.github.com>
2025-07-08 08:47:33 +0200

    first commit

---
+++ backend/database/configDB.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoDb: {
+    uri: "mongodb+srv://Ayden:M5xy2IWWclR7t3xt@cluster0.xgxfz3t.mongodb.net/todolist_render?retryWrites=true&w=majority&",
+  },
+};
---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const config = require("./database/configDB");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+//render
+const path = require("path");
+
+const __DIRNAME = path.resolve();
+
+app.use(express.static(path.join(__DIRNAME, "/client/dist")));
+
+const routes = require("./routes");
+
+app.use(routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+//end render
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const routes = require("./routes");
+app.use(routes);
+
+mongoose
+  .connect(config.mongoDb.uri)
+  .then(() => {
+    console.log("Connection mongoDB Ok");
+  })
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
